1c2f5a4be86126fdd040390dddc0129e2f769617
Alexey.Artemov <lexart.pub@ya.ru>
2022-06-24 17:29:48 +0500

    Added random color to the Example 007 Magic

--- example007_Magic/Program.cs
+++ example007_Magic/Program.cs
@@ -18,6 +18,7 @@ int x = xa, y = ya; //y = xb;
 int count = 0;
 
 int what = 0;
+int color = 0;
 
 while (count < 20000)
 {
@@ -45,6 +46,14 @@ while (count < 20000)
     Console.SetCursorPosition(x, y);
     Console.WriteLine("+");
     count++;
+    
+    if ((count % 100) != 0) // меняем цвет текста случайным образом каждые сто итераций
+    {
+        color = new Random().Next(1, 15);
+        Console.ForegroundColor = (ConsoleColor)color;
+    }
+
+Console.SetCursorPosition(1, yb);
 
 }
 // В итоге получается фрактальная фигура, т.н. треугольник Серпинского
